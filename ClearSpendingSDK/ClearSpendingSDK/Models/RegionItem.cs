e279062491d020505116c4696046f0ed822f7474
Anton Lenev <m0rg0t.Anton@gmail.com>
2014-04-21 21:16:33 +0400

    Load regions

---
+++ ClearSpendingSDK/ClearSpendingSDK/Models/RegionItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace ClearSpendingSDK.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RegionItem: ViewModelBase
+    {
+        public int SubjectCode { get; set; }
+        public string Name { get; set; }
+        public string CodeOKATO { get; set; }
+        public string CodeKLADR { get; set; }
+        public string CodeISO3166 { get; set; }
+        public string Id { get; set; }
+    }
+}
--- ClearSpendingSDK/ClearSpendingSDK/RegionsData.cs
+++ ClearSpendingSDK/ClearSpendingSDK/RegionsData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
@@ -7,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
 using Newtonsoft.Json.Linq;
+using ClearSpendingSDK.Models;
+using Newtonsoft.Json;
 
 namespace ClearSpendingSDK
 {
@@ -27,16 +30,31 @@ namespace ClearSpendingSDK
 
                 HttpResponseMessage result = await
                     http.GetAsync(
-                        new Uri("https://clearspending.p.mashape.com/v1/regions/select?regioncode=all"));
+                        new Uri("https://clearspending.p.mashape.com/v1/regions/select/?regioncode=all"));
                 string RawResult = await result.Content.ReadAsStringAsync();
                 JObject resultJObject = JObject.Parse(RawResult);
+                RegionItems = JsonConvert.DeserializeObject<ObservableCollection<RegionItem>>(resultJObject["regions"]["data"].ToString());
                 Debug.WriteLine(RawResult);
             }
             catch
             {
             }
-
             return true;
         }
+
+        private ObservableCollection<RegionItem> _regionItems = new ObservableCollection<RegionItem>();
+        /// <summary>
+        /// Список регионов
+        /// </summary>
+        public ObservableCollection<RegionItem> RegionItems
+        {
+            get { return _regionItems; }
+            set
+            {
+                _regionItems = value;
+                RaisePropertyChanged("RegionItems");
+            }
+        }
+        
     }
 }
